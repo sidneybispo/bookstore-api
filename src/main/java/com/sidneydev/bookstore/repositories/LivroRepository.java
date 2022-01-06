86efd044e249f9095305084c99d8acca23fd8f1d
sidneybispo <protecney@gmail.com>
2022-01-06 15:58:49 -0300

    repositories implementados

---
+++ src/main/java/com/sidneydev/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,10 @@
+package com.sidneydev.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.sidneydev.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
